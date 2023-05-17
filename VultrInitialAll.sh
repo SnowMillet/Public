68163651ead9984aa4fb44ef3a45e798c8bc3f22
SnowMillet <46582324+SnowMillet@users.noreply.github.com>
2023-05-17 09:41:21 +0000

    修改 VultrInitialAll.sh 中问题

--- VultrInitialAll.sh
+++ VultrInitialAll.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 # Vultr 初始化脚本，依次完成如下脚本
-# OutlineInstall.sh --> OutlineAccessTextToJson.sh --> OulineNotificationBySlack.sh
+# OutlineInstall.sh --> OutlineAccessTextToJson.sh --> OutlineNotificationBySlack.sh
 
+sudo wget -qO- https://raw.githubusercontent.com/SnowMillet/Public/main/OutlineInstall.sh | bash
+
+sudo wget -qO- https://raw.githubusercontent.com/SnowMillet/Public/main/OutlineAccessTextToJson.sh | bash
+
+sudo wget -qO- https://raw.githubusercontent.com/SnowMillet/Public/main/OutlineNotificationBySlack.sh | bash
