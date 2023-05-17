d10f8245b7510752a0292b9ef3bd008c7241dcd2
SnowMillet <46582324+SnowMillet@users.noreply.github.com>
2023-05-17 11:56:07 +0000

    修改 OutlineNotificationBySlack.sh

--- OutlineNotificationBySlack.sh
+++ OutlineNotificationBySlack.sh
@@ -5,4 +5,4 @@
 # 读取Outline访问信息，将其转换为JSON格式，并通过curl发送到指定URL
 access_content=$(sudo cat /opt/outline/access.txt)
 
-sudo curl -X POST -H "Content-Type: application/json" -d '{"text": "'"${access_content}"'"}' https://hooks.slack.com/services/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
+sudo curl -X POST -H "Content-Type: application/json" -d '{"text": "'"${access_content}"'"}' https://hooks.slack.com/services/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
